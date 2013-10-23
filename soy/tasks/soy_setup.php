3abd5b7ca6e63273f7fb1313a6c4b465306f6b1f
Pierrick <pierrick.varin@zagett.com>
2013-10-23 21:59:04 +0200

    Updated soy:setup task using $base_path

--- soy/tasks/soy_setup.php
+++ soy/tasks/soy_setup.php
@@ -1,28 +1,28 @@
 <?php
 
 Task('soy:setup', function() {
-	global $remote_server;
+	global $base_path;
 	select('remote_server');
 
 	announce('soy', 'Checking presence of required folders');
 	status();
 
-	if( ! test("-e {$remote_server['path']}") ) {
-		bash("mkdir {$remote_server['path']}");
+	if( ! test("-e $base_path") ) {
+		bash("mkdir $base_path");
 	} else {
 		announce('bash', '>> Base folder already exists', true);
 		status('ok');
 	}
 
-	if( ! test("-e {$remote_server['path']}/release") ) {
-		bash("mkdir {$remote_server['path']}/release");
+	if( ! test("-e $base_path/release") ) {
+		bash("mkdir $base_path/release");
 	} else {
 		announce('bash', '>> Release folder already exists', true);
 		status('ok');
 	}
 
-	if( ! test("-e {$remote_server['path']}/shared") ) {
-		bash("mkdir {$remote_server['path']}/shared");
+	if( ! test("-e $base_path/shared") ) {
+		bash("mkdir $base_path/shared");
 	} else {
 		announce('bash', '>> Shared folder already exists', true);
 		status('ok');
